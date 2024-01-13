502025c46010c6e9573274bcd434d882a5a44f82
kylestarrett1@gmail.com <kylestarrett1@gmail.com>
2024-01-13 00:45:08 -0600

    Updated header to have drawer. Added favorites screen.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function(api) {
+// Add Reanimated's babel plugin to your babel.config.js:
+// **Reanimated plugin has to be listed last.**
+// Ref. https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
